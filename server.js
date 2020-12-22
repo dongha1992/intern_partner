72d5befc18db821cd4ee88bf64631a775b2e4b08
dongha <dongha@email.com>
2020-12-23 03:41:31 +0900

    [fix] server.js 추가

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const next = require('next');
+const path = require('path');
+
+const dev = process.env.NODE_ENV !== 'production';
+const prod = process.env.NODE_ENV === 'production';
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use('/', express.static(path.join(__dirname, 'public')));
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
+
+  server.get('*', (req, res) => handle(req, res));
+
+  server.listen(prod ? process.env.PORT : 5700, () => {
+    console.log(`success ${prod ? process.env.PORT : 5700}`);
+  });
+});
