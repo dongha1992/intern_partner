2a5a0135a52b33a9e260feca235d02dafdc46a17
dongha <dongha@email.com>
2020-12-24 16:29:16 +0900

    [fix] git clone 후 에러 해결

---
+++ pages/user/register/employee/companySearch/index.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import { withRouter } from 'next/router';
+import Header from '../../../../../components/Header';
+import CompanySearchList from './CompanySearchList';
+import Button from '../../../../../components/Button';
+import { THEME_COLOR } from '../../../../../constants/Color';
+import styles from './CompanySearch.scss';
+import { COMPANY_SEARCH } from '../../../../../constants/employee/FormTitle';
+import { CONFIRM_BUTTON } from '../../../../../constants/employee/CompanySearchButton';
+
+@withRouter
+@inject('SearchCompanyStore')
+@observer
+class CompanySearch extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static async getInitialProps({ mobxStore }) {
+    return { mobxStore };
+  }
+  goToForm = () => {
+    console.log(this.props.router);
+    this.props.router.push('/user/register/employee');
+  };
+  render() {
+    const { SearchCompanyStore } = this.props;
+
+    return (
+      <div className={styles.company_search_container}>
+        <Header title={COMPANY_SEARCH} />
+        <div
+          className={styles.form_wrap}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignContent: 'space-around',
+            height: '694px',
+          }}>
+          <CompanySearchList />
+          <Button
+            value={CONFIRM_BUTTON}
+            margin={0}
+            searchInput={SearchCompanyStore.searchInput}
+            goToForm={() => {
+              this.props.router.push('/user/register/employee');
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CompanySearch;
