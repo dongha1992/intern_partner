b95d1caf7b9913784a7b56639d01c992927270ad
류지혜 <ryujihye@lyujihyeui-MacBook-Pro.local>
2020-12-29 13:46:53 +0900

    [finish] Main UI

--- components/Card/MainCard.js
+++ components/Card/MainCard.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import styles from './MainCard.scss';
 
-const MainCard = () => {
+const MainCard = ({ name, id, onClick, carType, carNumber, date }) => {
   return (
-    <div className={styles.call_container}>
+    <div className={styles.call_container} id={id}>
       <div className={styles.suggestion_container}>
         <div className={styles.suggestion}>제안</div>
-        <div className={styles.count}>{/* {el.count} */}1</div>
+        <div className={styles.count}>{id}</div>
       </div>
       <div>
         <div className={styles.company_suggestion}>공업사 요청</div>
         <div className={styles.main_card_row}>
           <span className={styles.customer_car_type_text}>고객차종</span>
-          <span className={styles.customer_car_type}>
-            {/* {el.type} */}
-            11
-          </span>
+          <span className={styles.customer_car_type}>{carType}</span>
         </div>
         <div className={styles.main_card_row}>
           <span className={styles.customer_car_number_text}>차량번호</span>
-          <span className={styles.customer_car_number}>-</span>
+          <span className={styles.customer_car_number}>{carNumber}</span>
         </div>
         <div className={styles.main_card_row}>
           <span className={styles.customer_call_date_text}>요청일</span>
-          <span className={styles.customer_call_date}>
-            {/* {el.date} */}
-            111
-          </span>
+          <span className={styles.customer_call_date}>{date}</span>
         </div>
       </div>
     </div>
--- pages/user/main/index.js
+++ pages/user/main/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { MainHeader } from '../../../components/Header';
 import MainTab from '../../../components/MainTab';
 import { MainCard } from '../../../components/Card';
@@ -7,19 +7,49 @@ import styles from './MainPage.scss';
 import useStore from '../../../stores';
 import { inject, observer } from 'mobx-react';
 import { useObserver } from 'mobx-react';
+import axios from 'axios';
 
 const { MainFooterActiveStore } = useStore();
-const MyCall = () => {
+
+const MyCall = ({ data }) => {
+  const [lists, setList] = useState(data);
+
+  const CardLists = lists.map((list) => {
+    return (
+      <MainCard
+        name={list.name}
+        id={list.id}
+        onClick={() => {
+          console.log('');
+        }}
+        key={list.id}
+        carType={list.car_type}
+        carNumber={list.car_number}
+        date={list.date}
+      />
+    );
+  });
+
   return useObserver(() => (
     <div className={styles.main_container}>
-      <MainHeader />
-      <MainTab />
-      <div className={styles.main_background}>
-        <MainCard />
+      <div className={styles.main_headerWrap}>
+        <MainHeader />
+        <MainTab />
       </div>
+      <div className={styles.main_background}>{CardLists}</div>
       <MainFooter />
     </div>
   ));
 };
+
+export async function getServerSideProps() {
+  const res = await axios.get('http://localhost:5700/api/getRequestInfo');
+  const data = res.data;
+  return {
+    props: {
+      data,
+    },
+  };
+}
 
 export default MyCall;
