e7956617b9096b90bebc3077df9d95f676f13fc0
김동하 <58624851+dongha1992@users.noreply.github.com>
2021-01-18 16:29:30 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const baseUrl = 'http://localhost:5700';
-export const SERVER_URL = 'http://52.79.55.158:8000';
-export const CHAT_SERVER = 'http://172.30.1.31:8000';
-export const CARLIST_API = '';
+export const SERVER_URL = '';
+export const CHAT_SERVER = '';
+
